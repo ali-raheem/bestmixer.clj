20173da9b8ee1367b2189b50f699f806dc8a7c28
Ali Raheem <ali@hadoken.cc>
2018-11-07 16:06:57 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# bestmixer
+
+A Clojure library designed to handle API calls to BextMixer.io
+
+## Usage
+
+
+
+## License
+
+Copyright © 2018 Ali Raheem
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject bestmixer "0.1.0-SNAPSHOT"
+  :description "clojure library for BestMixer.io API"
+  :url "https://github.com/ali-raheem/bestmixer.clj"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"],
+                 [clj-http "3.9.1"]
+                 [cheshire "5.8.1"]]
+  :main bestmixer.core)
